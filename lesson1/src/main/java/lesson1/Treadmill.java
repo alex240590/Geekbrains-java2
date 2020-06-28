a8eca56a60959448f9bd48275f3e339b97db9b86
alex240590 <lelek1@bk.ru>
2020-06-28 18:24:41 +0300

    [Урок 1] Объектно-ориентированное программирование Java

---
+++ lesson1/src/main/java/lesson1/Treadmill.java
@@ -0,0 +1,15 @@
+package lesson1;
+
+//Создаю класс Беговая дорожка, задаю ее длину, создаю геттер к длине
+
+public class Treadmill {
+
+   private int length = 50;
+
+
+   public int getLength() {
+       return length;
+   }
+
+
+}
